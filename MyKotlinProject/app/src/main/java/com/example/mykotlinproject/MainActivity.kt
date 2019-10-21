308a5d53c01fdc39fd788104165c77751201b55e
Rositaa99 <rositaafida99@gmail.com>
2019-10-21 18:47:23 +0700

    Ini tugasku

---
+++ MyKotlinProject/app/src/main/java/com/example/mykotlinproject/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.mykotlinproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.content.Intent
+import android.widget.EditText
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val NIM = findViewById<EditText>(R.id.editText)
+        val Nama = findViewById<EditText>(R.id.editText2)
+        val Nilai = findViewById<EditText>(R.id.editText3)
+
+        button.setOnClickListener{
+            if((editText.text.isEmpty())||editText2.text.isEmpty()||editText3.text.isEmpty()){
+                toast("Kolom tidak boleh kosong",Toast.LENGTH_LONG)
+            }else{
+                val nim = NIM.text.toString()
+                val nama = Nama.text.toString()
+                val nilai : Int = Integer.parseInt(Nilai.text.toString())
+                val intent = Intent(this@MainActivity, Main2Activity::class.java)
+                intent.putExtra("nim", nim)
+                intent.putExtra("nama",nama)
+                intent.putExtra("nilai",nilai)
+
+                startActivity(intent)
+            }
+        }
+    }
+
+    fun toast(message: String,length: Int=Toast.LENGTH_LONG){
+        Toast.makeText(this, message, length).show()
+    }
+}
+
+
